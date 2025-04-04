7bee67601c280efcc12714578fc5485a3c6d821f
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-04 16:48:48 +0300

    Define addingEventListener function and bind event listener

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+
 function addingEventListener() {
-}
+    
+    const input = document.getElementById('button');
+  
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to ensure the event listener is bound
+  addingEventListener();
+  
